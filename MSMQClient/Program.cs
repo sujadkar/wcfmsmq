100df66018445f5afbcc861d2c386f1629db7cbc
Sujay Adkar <sujayadkar@gmail.com>
2016-10-09 23:19:13 +0530

    Add project files.

---
+++ MSMQClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSMQClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MSMQServiceReference.ServiceClient client = new MSMQServiceReference.ServiceClient();
+			Console.WriteLine("Enter data to process");
+			var data = Console.ReadLine();
+
+			while(data != "Q")
+			{
+				client.Process(data);
+				Console.WriteLine("Enter data to process");
+				data = Console.ReadLine();
+			}
+		}
+	}
+}
---
+++ WCFMSMQ/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFMSMQ
+{
+	[ServiceContract]
+	interface IService
+	{
+		[OperationContract(IsOneWay =true)]
+		void Process(string data);
+	} 
+
+	[ServiceBehavior]
+	public class MSMQService : IService
+	{
+		[OperationBehavior]
+		public void Process(string data)
+		{
+			Console.WriteLine(String.Format("Process data {0} at {1}", data, DateTime.Now));
+		}
+	}
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceHost hotsname = new ServiceHost(typeof(MSMQService));
+			hotsname.Open();
+			Console.WriteLine("Service is ready");
+			Console.ReadLine();
+		}
+	}
+}
